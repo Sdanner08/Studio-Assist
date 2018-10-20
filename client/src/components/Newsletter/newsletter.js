6b18e17aa9a0c2927163db5c1f6457598c5f726c
Shane Danner <sdanner08@yahoo.com>
2018-10-20 11:21:43 -0400

    fixing glitch

--- client/src/components/Newsletter/newsletter.js
+++ client/src/components/Newsletter/newsletter.js
@@ -5,17 +5,28 @@ import "./newsletter.css";
 class Newsletter extends Component {
     render() {
         return (
-            <div id="dash" className="container-fluid">
-                <h1>News of the Week</h1>
-                <a href="https://placeholder.com"><img id="newImg" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/British_Women%E2%80%99s_Olympic_swimming_team%2C_London%2C_1948.jpg/1200px-British_Women%E2%80%99s_Olympic_swimming_team%2C_London%2C_1948.jpg" className="news-image" alt="Profile" /> </a>
-                <h6>New Classes starting next month</h6>
-                    <ul>
-                        <li>Kick Boxing</li>
-                        <li>Ballet</li>
-                        <li>Hot Yoga</li>
-                    </ul>
-                <h6>Vacations</h6>
-                <p>George will be going on vacation next week so all his class will be handled by Sabrina.</p>
+            <div id="dashSumm">
+                <div className="jumbotron jumbotron-fluid">
+                    <div className="container">
+                        <h1 className="display-4">Fluid jumbotron</h1>
+                        <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                    </div>
+                </div>
+
+                <div className="jumbotron jumbotron-fluid">
+                    <div className="container">
+                        <h1 className="display-4">Fluid jumbotron</h1>
+                        <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                    </div>
+                </div>
+
+                <div className="jumbotron jumbotron-fluid">
+                    <div className="container">
+                        <h1 className="display-4">Fluid jumbotron</h1>
+                        <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                    </div>
+                </div>
+
             </div>
         )
     }
