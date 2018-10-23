9812e79be8d7b37018cfdca5d1ef76203f22715b
saajan94 <saajan1994@aol.com>
2018-10-23 17:01:46 -0400

    Add alert for attendance

--- client/src/components/Attendance/Attendance.js
+++ client/src/components/Attendance/Attendance.js
@@ -26,11 +26,11 @@ const Attendance = (props) => {
     }
     return (
         <div className="card mx-auto attendanceTable mt-4 mb-4">
-            <div class="row p-3">
-                <div class="col-md-12">
+            <div className="row p-3">
+                <div className="col-md-12">
                     <h1 className="display-4 text-center">Attendance</h1>
                     <h5 className="mt-4 text-center">Today's Date: {today}</h5>
-                    <table class="table table-striped">
+                    <table className="table table-striped">
                         <thead>
                             <tr>
                                 <th scope="col">First Name</th>
---
+++ client/src/components/AttendanceAlert/AttendanceAlert.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AttendanceAlert = (props) => {
+    return (
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Today's attendance has been successfully submitted!</strong>
+            <button type="button" className="close" data-dismiss="alert" onClick={props.onClose} aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    )
+}
+
+export default AttendanceAlert;
+
